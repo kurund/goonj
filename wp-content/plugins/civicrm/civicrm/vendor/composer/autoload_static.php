1a259d6e3d70ae7a5010268d9519314c6950c2b4
shubtiwari <Shubham.tiwari@coloredcow.com>
2024-06-13 18:21:13 +0530

    add latest plugin

--- wp-content/plugins/civicrm/civicrm/vendor/composer/autoload_static.php
+++ wp-content/plugins/civicrm/civicrm/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit917fc172acfa94207589e6aa93887a01
+class ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce
 {
     public static $files = array (
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
@@ -83,6 +83,7 @@ class ComposerStaticInit917fc172acfa94207589e6aa93887a01
         '5897ea0ac4cccf14d323035e65887801' => __DIR__ . '/..' . '/symfony/polyfill-php82/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'bad842bce63596a608e2623519fb382c' => __DIR__ . '/..' . '/xkerman/restricted-unserialize/src/function.php',
+        '23c7c9fb739a56c448f3bc838e8b186d' => __DIR__ . '/../..' . '/functions.php',
         '59969633dcdb4ee3e2fec945065028d6' => __DIR__ . '/../..' . '/guzzle_php81_shim.php',
     );
 
@@ -713,11 +714,11 @@ class ComposerStaticInit917fc172acfa94207589e6aa93887a01
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit917fc172acfa94207589e6aa93887a01::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit917fc172acfa94207589e6aa93887a01::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit917fc172acfa94207589e6aa93887a01::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInit917fc172acfa94207589e6aa93887a01::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInit917fc172acfa94207589e6aa93887a01::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInit928c00fe2748ea0ce3614dfdd1f3f7ce::$classMap;
 
         }, null, ClassLoader::class);
     }
