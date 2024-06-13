18b1cb945c7c83fff86c465031b24f618fee22c2
shubtiwari <Shubham.tiwari@coloredcow.com>
2024-06-13 17:20:58 +0530

    first commit

---
+++ wp-content/plugins/civicrm/civicrm/civicrm-version.php
@@ -0,0 +1,8 @@
+<?php
+/** @deprecated */
+function civicrmVersion( ) {
+  return array( 'version'  => '5.73.2',
+                'cms'      => 'Wordpress',
+                'revision' => '' );
+}
+
