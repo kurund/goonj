cd34120778a5f782264e6f075ce5a0a6420012b9
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-01 15:05:50 +0530

    added changes for accessing staging crdential from github secrets

--- e2e/admin-login.spec.js
+++ e2e/admin-login.spec.js
@@ -3,28 +3,29 @@ const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
   // @ts-ignore
-  await page.goto(process.env.BASE_URL);
-
-  // Use environment variables for username and password
   const username = process.env.USERNAME;
   const password = process.env.PASSWORD;
+  // @ts-ignore
+  await page.goto(process.env.BASE_URL_ADMIN_SITE);
+  await expect(page).toHaveURL('/wp-login');
 
-  // await page.goto('https://goonj-crm.staging.coloredcow.com/wp-admin');
-  const currentURL = page.url();
-  const expectedSubstring = '/wp-login';
-  if (currentURL.includes(expectedSubstring)) {
-    console.log('URL contains the expected substring.');
-  } else {
-    console.log('URL does not contain the expected substring.');
-  }
-
+  // Use environment variables for username and password
+  
+  // const currentURL = page.url();
+  // const expectedSubstring = '/wp-login';
+  // if (currentURL.includes(expectedSubstring)) {
+  //   console.log('URL contains the expected substring.');
+  // } else {
+  //   console.log('URL does not contain the expected substring.');
+  // }
   const usernameField = page.locator('#user_login');
   // @ts-ignore
-  await usernameField.fill(username); // Replace 'admin' with the actual admin username
+  await usernameField.fill(username); 
   // @ts-ignore
-  await page.fill('#user_pass', password); // Replace 'password123' with the actual password
+  await page.fill('#user_pass', password); 
   // Click the login button
-  await page.click('#wp-submit'); // Replace with the actual button selector
+  await page.click('#wp-submit'); 
+  await expect(page).toHaveURL('/wp-admin');
 
 });
 
