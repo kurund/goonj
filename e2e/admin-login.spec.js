ed85e9ee328c3bd7be452023c1d580ce5ae58fbf
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-02 11:17:45 +0530

    updated changes for running test cases on github actions

--- e2e/admin-login.spec.js
+++ e2e/admin-login.spec.js
@@ -1,22 +1,16 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test('Login into admin site ', async ({ page }) => {
-  // @ts-ignore
   const baseURL = process.env.BASE_URL_ADMIN_SITE
   const username = process.env.USERNAME;
   const password = process.env.PASSWORD;
-  // @ts-ignore
-  await page.goto('/');
-
-  // Use environment variables for username and password
+  await page.goto(baseURL);
+  expect(page.url()).toContain('/wp-login');
   const usernameField = page.locator('#user_login');
-  // @ts-ignore
   await usernameField.fill(username); 
-  // @ts-ignore
   await page.fill('#user_pass', password); 
-  // Click the login button
-  await page.click('#wp-submit'); 
+  await page.click('#wp-submit');
+  expect(page.url()).toContain('/wp-admin');
 
 });
 
