2e79fcff3a8cd74f23b06d9130b516226e6fa1b8
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-08 00:22:53 +0530

    added code to input foem fields

--- play-wright/e2e/individual-registration.spec.js
+++ play-wright/e2e/individual-registration.spec.js
@@ -1,46 +1,32 @@
 import { test } from '@playwright/test';
 import { IndividualRegistrationPage } from '../pages/individual-registration.page'; 
 
-
-async function selectDropdownOptionByText(page, dropdownInputId, optionText) {
-    // Click on the dropdown to open it
-    const dropdownSelector = `div#s2id_${dropdownInputId.split('-').pop()}`;
+async function selectDropdownOption(page, dropdownSelector, searchTerm) {
+    // Click the dropdown to activate it
     await page.click(dropdownSelector);
-  
-    // Wait for the dropdown options to be visible
-    await page.waitForSelector('.select2-results li');
-  
-    // Construct the selector for the desired option based on its text
-    const optionSelector = `.select2-results li:has-text("${optionText}")`;
-  
-    // Click on the desired option
+    // Input the search term
+    await page.fill('#s2id_autogen2_search', searchTerm);
+    // Click the desired option
+    const optionSelector = `.select2-result-label:text("${searchTerm}")`;
     await page.click(optionSelector);
+    // Press Enter to select the option
+    await page.press('.select2-input', 'Enter');
+    await page.keyboard.press('Tab');
 }
-
 
 test('get appended URL test', async ({ page }) => {
   const individualRegistrationPage = new IndividualRegistrationPage(page);
-  
   // Get the appended URL
   const baseUrl = individualRegistrationPage.getAppendedUrl('/Registration');
-  // Navigate to the appended URL
   await page.goto(baseUrl);
-  await page.waitForTimeout(4000)
-//   individualRegistrationPage.selectNameInitialDropdownOption('Mr')
- await page.click('.form-group .select2-choice');
- await page.keyboard.press('Tab');
-// await selectDropdownOptionByText(page, '#prefix-id-1', 'Mr.'); // Example: Selects 'Mrs.'
- await page.waitForTimeout(2000)
-
- // Input text into the first name field
- await page.fill('input#first-name-2', 'John');
-//  individualRegistrationPage.enterFirstName('DEEPAK')
-//  individualRegistrationPage.enterEmail('deepak@mail.com')
-//  individualRegistrationPage.enterLastName('SINGH')
-await page.fill('input#last-name-3', 'singh');
-await page.fill('input#email-4', 'johnsingh@mail.com');
-await page.fill('input#phone-6', '1234567890');
-
+  await page.waitForTimeout(1000)
+  await selectDropdownOption(page, '.select2-container', 'Ms.');
+ individualRegistrationPage.enterFirstName('DEEPAK')
+ await page.waitForTimeout(200); 
+  individualRegistrationPage.enterLastName('SINGH')
+  await page.waitForTimeout(200); 
+ individualRegistrationPage.enterEmail('deepak@mail.com')
+ await page.waitForTimeout(200); 
 
 });
 
