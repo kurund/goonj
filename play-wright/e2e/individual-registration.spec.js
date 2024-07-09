aaf491cc44651dba7a1eda97024cfd81047ee11d
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-09 22:56:47 +0530

    updated the input fields code

--- play-wright/e2e/individual-registration.spec.js
+++ play-wright/e2e/individual-registration.spec.js
@@ -1,6 +1,8 @@
-import { test } from '@playwright/test';
+import { test, expect  } from '@playwright/test';
 import { IndividualRegistrationPage } from '../pages/individual-registration.page'; 
+import { userLogin } from '../utils';
 const userDetails = require('../fixture/user-details.json');
+
 
 test('submit the basic registration form', async ({ page }) => {
   const individualRegistrationPage = new IndividualRegistrationPage(page);
@@ -25,16 +27,28 @@ test('submit the basic registration form', async ({ page }) => {
   await page.waitForTimeout(400); 
   individualRegistrationPage.enterPostalCode(userDetails.postalCode)
   await individualRegistrationPage.selectDropdownOption('#select2-chosen-1', '#s2id_autogen1_search', userDetails.state);
-  await individualRegistrationPage.selectRadioButton(userDetails.radioOption);
+  // await individualRegistrationPage.selectRadioButton(userDetails.radioOption);
   await page.waitForTimeout(2000); 
-  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen5', '#s2id_autogen5', userDetails.activityInterested); 
-  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen6', '#s2id_autogen6', userDetails.voluntarySkills); 
-  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen7', '#s2id_autogen7', userDetails.volunteerMotivation);
-  // await individualRegistrationPage.selectDropdownOption('#select2-chosen-8', '#s2id_autogen8_search', userDetails.volunteerHours);
-  // individualRegistrationPage.enterProfession(userDetails.profession)
+  await individualRegistrationPage.selectDropdownOption('#s2id_autogen5', '#s2id_autogen5', userDetails.activityInterested); 
+  await individualRegistrationPage.selectDropdownOption('#s2id_autogen6', '#s2id_autogen6', userDetails.voluntarySkills); 
+  await individualRegistrationPage.enterOtherSkills('writing')
+  await individualRegistrationPage.selectDropdownOption('#s2id_autogen7', '#s2id_autogen7', userDetails.volunteerMotivation);
+  await individualRegistrationPage.selectDropdownOption('#select2-chosen-8', '#s2id_autogen8_search', userDetails.volunteerHours);
+  individualRegistrationPage.enterProfession(userDetails.profession)
   await page.waitForTimeout(400);  
-  // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.');
-  // await individualRegistrationPage.clickSubmitButton();
+  // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.'); // This code would be required for required field message
+  await individualRegistrationPage.clickSubmitButton();
   await page.waitForTimeout(1000)
 });
 
+test('check user details on dashboard', async ({ page }) => {
+  const individualRegistrationPage = new IndividualRegistrationPage(page);
+  await userLogin(page);
+    // Click on the Volunteers tab
+  await page.click('a:has-text("Volunteers")');
+  // Wait for the submenu to appear and click on "New Signups"
+  await page.waitForSelector('a:has-text("New Signups")');
+  await page.click('a:has-text("New Signups")');
+  await page.waitForTimeout(3000) //added wait to load the data of new signup
+  //Need to add code to check the records for the user details
+});
