e8c78420fe6bd062c641b21063a8f093d40318e0
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-09 22:52:14 +0530

    removed uncommented code

--- play-wright/e2e/admin-login.spec.js
+++ play-wright/e2e/admin-login.spec.js
@@ -5,15 +5,15 @@ test('Login as goonj admin user', async ({ page }) => {
   const username = process.env.USERNAME;
   const password = process.env.PASSWORD;
   await page.goto(baseURL);
-  // await page.fill('#user_login', username); 
-  // await page.fill('#user_pass', password); 
-  // await page.click('#wp-submit');
-  // expect(page.url()).toContain('page=CiviCRM');
-  // await page.waitForSelector('.crm-title .title');
-  // //Get the text content of the CRM title
-  // const crmTitleText = await page.textContent('.crm-title .title');
-  // expect(crmTitleText.trim()).toBe('CiviCRM Home');
-  // const volunteersTab =  page.locator('.wp-submenu .wp-submenu-head:has-text("Volunteers")');
-  // await expect(volunteersTab).toHaveText('Volunteers');
+  await page.fill('#user_login', username); 
+  await page.fill('#user_pass', password); 
+  await page.click('#wp-submit');
+  expect(page.url()).toContain('page=CiviCRM');
+  await page.waitForSelector('.crm-title .title');
+  //Get the text content of the CRM title
+  const crmTitleText = await page.textContent('.crm-title .title');
+  expect(crmTitleText.trim()).toBe('CiviCRM Home');
+  const volunteersTab =  page.locator('.wp-submenu .wp-submenu-head:has-text("Volunteers")');
+  await expect(volunteersTab).toHaveText('Volunteers');
 });
 
