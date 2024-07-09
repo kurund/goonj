372b07286d919bf7547c0c1aa29b547fdce05a15
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-09 15:29:59 +0530

    added code to fecth user details by fixture

--- play-wright/e2e/individual-registration.spec.js
+++ play-wright/e2e/individual-registration.spec.js
@@ -1,38 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { IndividualRegistrationPage } from '../pages/individual-registration.page'; 
+const userDetails = require('../fixture/user-details.json');
 
 test('submit the basic registration form', async ({ page }) => {
   const individualRegistrationPage = new IndividualRegistrationPage(page);
 // Get the appended URL
-  const baseUrl = individualRegistrationPage.getAppendedUrl('/Registration');
+  const baseUrl = individualRegistrationPage.getAppendedUrl('volunteer-registration/');
   await page.goto(baseUrl);
   await page.waitForTimeout(1000)
   await individualRegistrationPage.selectDropdownOption('#select2-chosen-2', '#s2id_autogen2_search', 'Ms.');
-  individualRegistrationPage.enterFirstName('DEEPAK')
+  individualRegistrationPage.enterFirstName(userDetails.firstName)
   await page.waitForTimeout(200); 
-  individualRegistrationPage.enterLastName('SINGH')
+  individualRegistrationPage.enterLastName(userDetails.lastName)
   await page.waitForTimeout(200); 
-  individualRegistrationPage.enterEmail('deepak@mail.com')
+  individualRegistrationPage.enterEmail(userDetails.email)
   await page.waitForTimeout(200); 
-  individualRegistrationPage.enterMobileNumber('1234567890')
+  individualRegistrationPage.enterMobileNumber(userDetails.mobileNumber)
   await page.waitForTimeout(200); 
   await individualRegistrationPage.selectDropdownOption('#select2-chosen-3', '#s2id_autogen3_search', 'Male');
-  individualRegistrationPage.enterStreetAddress('56, dwarka, sector-7')
+  individualRegistrationPage.enterStreetAddress(userDetails.streetAddress)
   await page.waitForTimeout(400); 
-  individualRegistrationPage.enterCityName('delhi')
+  individualRegistrationPage.enterCityName(userDetails.cityName)
   await page.waitForTimeout(400); 
-  await individualRegistrationPage.selectDropdownOption('#select2-chosen-1', '#s2id_autogen1_search', 'Delhi');
-  individualRegistrationPage.enterPostalCode('110070')
-  await individualRegistrationPage.selectDropdownOption('#select2-chosen-4', '#s2id_autogen4_search', 'India');
-  await individualRegistrationPage.selectRadioButton('Yes');
+  // await individualRegistrationPage.selectDropdownOption('#select2-chosen-1', '#s2id_autogen1_search', userDetails.state);
+  individualRegistrationPage.enterPostalCode(userDetails.postalCode)
+  await individualRegistrationPage.selectDropdownOption('#select2-chosen-4', '#s2id_autogen4_search', userDetails.country);
+  await individualRegistrationPage.selectRadioButton(userDetails.radioOption);
   await page.waitForTimeout(2000); 
-//   await individualRegistrationPage.selectDropdownOption('#s2id_autogen5', '#s2id_autogen5', 'Raise funds'); 
-//   await individualRegistrationPage.selectDropdownOption('#s2id_autogen6', '#s2id_autogen6', 'Marketing'); 
-//   await individualRegistrationPage.selectDropdownOption('#s2id_autogen7', '#s2id_autogen7', 'Learn new skills');
-  await individualRegistrationPage.selectDropdownOption('#select2-chosen-8', '#s2id_autogen8_search', '2 to 6 hours daily');
-  individualRegistrationPage.enterProfession('Devops engineer')
+  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen5', '#s2id_autogen5', userDetails.activityInterested); 
+  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen6', '#s2id_autogen6', userDetails.voluntarySkills); 
+  // await individualRegistrationPage.selectDropdownOption('#s2id_autogen7', '#s2id_autogen7', userDetails.volunteerMotivation);
+  // await individualRegistrationPage.selectDropdownOption('#select2-chosen-8', '#s2id_autogen8_search', userDetails.volunteerHours);
+  individualRegistrationPage.enterProfession(userDetails.profession)
   await page.waitForTimeout(400);  
-  await individualRegistrationPage.handleDialogMessage('Please fill all required fields.');
+  // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.');
   await individualRegistrationPage.clickSubmitButton();
   await page.waitForTimeout(1000)
 });
