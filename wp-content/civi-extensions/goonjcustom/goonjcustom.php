614ee01d398545c8e1ded8f244f0463c17719523
pokhiii <pokhi@coloredcow.com>
2024-07-09 11:14:04 +0530

    register induction details

--- wp-content/civi-extensions/goonjcustom/goonjcustom.php
+++ wp-content/civi-extensions/goonjcustom/goonjcustom.php
@@ -3,6 +3,9 @@
 require_once 'goonjcustom.civix.php';
 
 use CRM_Goonjcustom_ExtensionUtil as E;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 
 /**
  * Implements hook_civicrm_config().
@@ -29,4 +32,32 @@ function goonjcustom_civicrm_install(): void {
  */
 function goonjcustom_civicrm_enable(): void {
   _goonjcustom_civix_civicrm_enable();
+}
+
+/**
+ * Add token services to the container.
+ *
+ * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+ */
+function goonjcustom_civicrm_container(ContainerBuilder $container) {
+  $container->addResource(new FileResource(__FILE__));
+  $container->findDefinition('dispatcher')->addMethodCall('addListener',
+    ['civi.token.list', 'goonjcustom_register_tokens']
+  )->setPublic(TRUE);
+  $container->findDefinition('dispatcher')->addMethodCall('addListener',
+    ['civi.token.eval', 'goonjcustom_evaluate_tokens']
+  )->setPublic(TRUE);
+}
+
+function goonjcustom_register_tokens(\Civi\Token\Event\TokenRegisterEvent $e) {
+  $e->entity('contact')
+    ->register('inductionDetails', ts('Induction details'));
+}
+
+function goonjcustom_evaluate_tokens(\Civi\Token\Event\TokenValueEvent $e) {
+  foreach ($e->getRows() as $row) {
+    /** @var TokenRow $row */
+    $row->format('text/html');
+    $row->tokens('contact', 'inductionDetails', 'induction details to come here' . $row->context['contactId']);
+  }
 }
