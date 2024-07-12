afa0eb9ea18975e83ce74913d009f02b097cba03
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-13 00:29:12 +0530

    added code for serach selected user

--- play-wright/e2e/individual-registration.spec.js
+++ play-wright/e2e/individual-registration.spec.js
@@ -1,27 +1,38 @@
-import { test, expect  } from '@playwright/test';
-import { IndividualRegistrationPage } from '../pages/individual-registration.page'; 
-import { userLogin } from '../utils';
-const userDetails = require('../fixture/user-details.json');
+import { test, expect } from '@playwright/test';
+import { IndividualRegistrationPage } from '../pages/individual-registration.page';
+import { userDetails,userFirstName, userEmail, userMobileNumber, userLogin } from '../utils.js';
 
-// Currently we are have implemented the registration with single user delete will need to
-// add functionality to delete user from admin dashboard or use faker library to generate new user in registration
+async function selectOptionFromDropdown(page, dropdownSelector, optionText) {
+  // Click to open the dropdown
+  await page.click(`${dropdownSelector} .select2-choice`);
 
+  // Select the desired option
+  const option = await page.locator(`.select2-result-label:has-text("${optionText}")`);
+  await option.click();
+}
+
+async function clickSubmitButton(page) {
+  // Locate the submit button by its role and click it
+  await page.getByRole('button', { name: /search/i }).click();
+}
 
 test('submit the basic registration form', async ({ page }) => {
   const individualRegistrationPage = new IndividualRegistrationPage(page);
-// Get the appended URL
-  const baseUrl = individualRegistrationPage.getAppendedUrl('/volunteer-registration/');
-  await page.goto(baseUrl);
-  await page.waitForTimeout(1000)
-  await individualRegistrationPage.selectTitle('Mr.');
-  await individualRegistrationPage.enterFirstName(userDetails.firstName);
+
+  // Get the appended URL
+  const vounteerURl = individualRegistrationPage.getAppendedUrl('/volunteer-registration/');
+  await page.goto(vounteerURl);
+  await page.waitForTimeout(1000);
+  await individualRegistrationPage.selectTitle(userDetails.nameInital);
+  await page.waitForTimeout(200);
+  await individualRegistrationPage.enterFirstName(userFirstName);
   await page.waitForTimeout(200);
   await individualRegistrationPage.enterLastName(userDetails.lastName);
   await page.waitForTimeout(200);
-  await individualRegistrationPage.enterEmail(userDetails.email);
+  await individualRegistrationPage.enterEmail(userEmail);
   await page.waitForTimeout(200);
   await individualRegistrationPage.selectCountry(userDetails.country);
-  await individualRegistrationPage.enterMobileNumber(userDetails.mobileNumber);
+  await individualRegistrationPage.enterMobileNumber(userMobileNumber);
   await page.waitForTimeout(200);
   await individualRegistrationPage.selectGender(userDetails.gender);
   await individualRegistrationPage.enterStreetAddress(userDetails.streetAddress);
@@ -31,7 +42,7 @@ test('submit the basic registration form', async ({ page }) => {
   await individualRegistrationPage.enterPostalCode(userDetails.postalCode);
   await individualRegistrationPage.selectState(userDetails.state);
   // await individualRegistrationPage.selectRadioButton(userDetails.radioOption);
-  // await page.waitForTimeout(2000);  
+  // await page.waitForTimeout(2000);
   await individualRegistrationPage.selectActivityInterested(userDetails.activityInterested);
   await individualRegistrationPage.selectVoluntarySkills(userDetails.voluntarySkills);
   await individualRegistrationPage.enterOtherSkills(userDetails.otherSkills);
@@ -39,19 +50,71 @@ test('submit the basic registration form', async ({ page }) => {
   await individualRegistrationPage.selectVolunteerHours(userDetails.volunteerHours);
   await individualRegistrationPage.enterProfession(userDetails.profession);
   await page.waitForTimeout(400);
-  // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.'); // This code would be required for required field message
+  // // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.'); // This code would be required for required field message
   await individualRegistrationPage.clickSubmitButton();
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(2000);
+  console.log(`Registered email: ${userEmail}`);
+  console.log(`Registered mobile number: ${userMobileNumber}`);
+  await userLogin(page);
+  // Click on the Volunteers tab
+  await page.click('a:has-text("Volunteers")');
+  await page.click('[data-name="Search"]');
+  // Click on 'Find Contacts' within the submenu
+  await page.click('[data-name="Find Contacts"] a', {force : true});
+  await page.waitForTimeout(2000)
+  await page.fill('input#sort_name', 'Himani')
+  // await page.fill('input#sort_name', userFirstName)
+  await selectOptionFromDropdown(page, '#s2id_contact_type', 'Individual');
+  await clickSubmitButton(page)
+  // You can add further assertions or actions here
+  // Example: verify the selected value
+  // const selectedValue = await page.locator('#s2id_contact_type .select2-chosen').innerText();
+  // expect(selectedValue).toBe('Individual');
 });
 
-// test('check user details on dashboard', async ({ page }) => {
+
+// import { test, expect  } from '@playwright/test';
+// import { IndividualRegistrationPage } from '../pages/individual-registration.page'; 
+// import { userLogin } from '../utils';
+// const userDetails = require('../fixture/user-details.json');
+
+
+// test('submit the basic registration form', async ({ page }) => {
 //   const individualRegistrationPage = new IndividualRegistrationPage(page);
-//   await userLogin(page);
-//     // Click on the Volunteers tab
-//   await page.click('a:has-text("Volunteers")');
-//   // Wait for the submenu to appear and click on "New Signups"
-//   await page.waitForSelector('a:has-text("New Signups")');
-//   await page.click('a:has-text("New Signups")');
-//   await page.waitForTimeout(3000) //added wait to load the data of new signup
-//   //Need to add code to check the records for the user details
+// // Get the appended URL
+//   const baseUrl = individualRegistrationPage.getAppendedUrl('/volunteer-registration/');
+//   await page.goto(baseUrl);
+//   await page.waitForTimeout(1000)
+//   await individualRegistrationPage.selectTitle('Mr.');
+//   await individualRegistrationPage.enterFirstName(userDetails.firstName);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.enterLastName(userDetails.lastName);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.enterEmail(userDetails.email);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.selectCountry(userDetails.country);
+//   await individualRegistrationPage.enterMobileNumber(userDetails.mobileNumber);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.selectGender(userDetails.gender);
+//   await individualRegistrationPage.enterStreetAddress(userDetails.streetAddress);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.enterCityName(userDetails.cityName);
+//   await page.waitForTimeout(200);
+//   await individualRegistrationPage.enterPostalCode(userDetails.postalCode);
+//   await individualRegistrationPage.selectState(userDetails.state);
+//   // await individualRegistrationPage.selectRadioButton(userDetails.radioOption);
+//   // await page.waitForTimeout(2000);  
+//   await individualRegistrationPage.selectActivityInterested(userDetails.activityInterested);
+//   await individualRegistrationPage.selectVoluntarySkills(userDetails.voluntarySkills);
+//   await individualRegistrationPage.enterOtherSkills(userDetails.otherSkills);
+//   await individualRegistrationPage.selectVolunteerMotivation(userDetails.volunteerMotivation);
+//   await individualRegistrationPage.selectVolunteerHours(userDetails.volunteerHours);
+//   await individualRegistrationPage.enterProfession(userDetails.profession);
+//   await page.waitForTimeout(400);
+//   // await individualRegistrationPage.handleDialogMessage('Please fill all required fields.'); // This code would be required for required field message
+//   await individualRegistrationPage.clickSubmitButton();
+//   await page.waitForTimeout(1000);
 // });
+
+// Currently we are have implemented the registeration with single user delete will need to
+// add functionality to delete user from admin dashboard or use faker library to generate new user inr
