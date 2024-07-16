0afd6225284fbf19487e116f210e4e1f8937bf6d
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-16 17:03:18 +0530

    added changes for serach contact page

--- play-wright/e2e/volunteer-registration.spec.js
+++ play-wright/e2e/volunteer-registration.spec.js
@@ -1,21 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { VolunteerRegistrationPage } from '../pages/volunteer-registration.page';
+import { SearchContactsPage } from '../pages/search-contact.page';
 import { userDetails,userFirstName, userEmail, userMobileNumber, userLogin } from '../utils.js';
-
-async function selectContactTypeDropdown(page, dropdownSelector, optionText) {
-  await page.click(`${dropdownSelector} .select2-choice`);
-  const option = await page.locator(`.select2-result-label:has-text("${optionText}")`);
-  await option.click();
-}
-
-async function clickSubmitButton(page) {
-  // Locate the submit button by its role and click it
-  await page.getByRole('button', { name: /search/i }).click();
-}
 
 test('submit the volunteer registration form and confirm on admin', async ({ page }) => {
   const volunteerRegistrationPage = new VolunteerRegistrationPage(page);
-
+  const searchContactsPage = new SearchContactsPage (page);
   // Get the appended URL
   const vounteerURl = volunteerRegistrationPage.getAppendedUrl('/volunteer-registration/');
   await page.goto(vounteerURl);
@@ -51,15 +41,12 @@ test('submit the volunteer registration form and confirm on admin', async ({ pag
   await page.waitForTimeout(2000);  //added wait as page was taking time load 
   await userLogin(page);
   // Click on the Volunteers tab
-  await page.click('a:has-text("Volunteers")');
-  await page.click('[data-name="Search"]');
-  // Click on 'Find Contacts' within the submenu
-  await page.click('[data-name="Find Contacts"] a', {force : true});
-  await page.waitForTimeout(2000)
-  await page.fill('input#sort_name', userEmail)
-  await selectContactTypeDropdown(page, '#s2id_contact_type', 'Individual');
-  await clickSubmitButton(page)
-  await page.waitForTimeout(2000)
+  await searchContactsPage.clickSearchLabel()
+  await searchContactsPage.clickFindContacts()
+  await searchContactsPage.inputUserNameOrEmail(userEmail)
+  await searchContactsPage.selectContactType('Individual');
+  await searchContactsPage.clickSearchButton();
+  await page.waitForTimeout(2000) //added wait as page was taking time load 
   const contactRowSelector = `table tbody tr:has(span[title="${userEmail}"])`;
   await page.waitForTimeout(1000)
   expect(contactRowSelector).toContain(userEmail)
