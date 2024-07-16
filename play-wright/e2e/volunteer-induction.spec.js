1d5d80df8f9a6ad519d5c2c45964f277896b6abf
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-16 17:11:54 +0530

    added changes for the serach contact

--- play-wright/e2e/volunteer-induction.spec.js
+++ play-wright/e2e/volunteer-induction.spec.js
@@ -1,17 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { VolunteerRegistrationPage } from '../pages/volunteer-registration.page';
 import { userDetails,userFirstName, userEmail, userMobileNumber, userLogin } from '../utils.js';
-
-async function selectContactTypeDropdown(page, dropdownSelector, optionText) {
-  await page.click(`${dropdownSelector} .select2-choice`);
-  const option = await page.locator(`.select2-result-label:has-text("${optionText}")`);
-  await option.click();
-}
-
-async function clickSubmitButton(page) {
-  // Locate the submit button by its role and click it
-  await page.getByRole('button', { name: /search/i }).click();
-}
+import { SearchContactsPage } from '../pages/search-contact.page';
 
 async function selectRecordActivityOption(page, option) {
     // Define mapping for activity options to selectors
@@ -74,6 +64,7 @@ async function clickVolunteerSuboption(page, option) {
 
 test('schedule induction and update induction status as completed', async ({ page }) => {
   const volunteerRegistrationPage = new VolunteerRegistrationPage(page);
+  const searchContactsPage = new SearchContactsPage (page);
   // Get the appended URL
   const vounteerURl = volunteerRegistrationPage.getAppendedUrl('/volunteer-registration/');
   await page.goto(vounteerURl);
@@ -108,18 +99,14 @@ test('schedule induction and update induction status as completed', async ({ pag
   await volunteerRegistrationPage.clickSubmitButton();
   await page.waitForTimeout(2000);
   await userLogin(page);
-  //Click on the Volunteers tab
-  await page.click('a:has-text("Volunteers")');
-  await page.click('[data-name="Search"]');
-  // Click on 'Find Contacts' within the submenu
-  await page.click('[data-name="Find Contacts"] a', {force : true});
-  await page.waitForTimeout(2000)
-  await page.fill('input#sort_name', userEmail)
-  await selectContactTypeDropdown(page, '#s2id_contact_type', 'Individual');
-  await clickSubmitButton(page)
+  await searchContactsPage.clickSearchLabel()
+  await searchContactsPage.clickFindContacts()
+  await searchContactsPage.inputUserNameOrEmail(userEmail)
+  await searchContactsPage.selectContactType('Individual');
+  await searchContactsPage.clickSearchButton();
   await page.waitForTimeout(2000)
   page.locator('a.view-contact').click({force: true})
-  //commented below code as flow is modified but can be used in other scenarios
+//commented below code as flow is modified but can be used in other scenarios
 //   await page.click('#crm-contact-actions-link')
 //   await selectRecordActivityOption(page, 'Induction');
 //   await page.waitForTimeout(2000)
