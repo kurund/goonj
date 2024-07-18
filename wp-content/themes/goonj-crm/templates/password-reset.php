bc9729e3e58b95264f179c463e8e05858ff69845
Tarun <tarun.joshi@coloredcow.com>
2024-07-18 10:53:13 +0530

    fix the feedbacks

--- wp-content/themes/goonj-crm/templates/password-reset.php
+++ wp-content/themes/goonj-crm/templates/password-reset.php
@@ -88,7 +88,7 @@ wp_enqueue_script( 'utils' );
 wp_enqueue_script( 'user-profile' );
 
 ?>
-<form name="resetpassform" id="resetpassform" action="<?php echo esc_url( home_url( '/password-confirmation/' ) ); ?>" method="post" autocomplete="off">
+<form name="resetpassform" id="resetpassform" action="<?php echo esc_url( network_site_url( 'wp-login.php?action=resetpass', 'login_post' ) ); ?>" method="post" autocomplete="off">
 	<div class="w-150 m-auto mb-35">
 		<a href="<?php echo home_url(); ?>">
 			<img class="w-150" src="<?php echo get_stylesheet_directory_uri(); ?>/images/goonj-logo.png" alt="Goonj-logo">
