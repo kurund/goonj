617bb1ec64765d32841fbf4705af75a80194f415
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-25 16:30:41 +0530

    updated default test case timeout to 300 seonds

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ require('dotenv').config({path: '.env'});
 module.exports = defineConfig({
   testDir: './play-wright/e2e',
   // Set default timeout for the test case
-  timeout: 240 * 1000,
+  timeout: 300 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
