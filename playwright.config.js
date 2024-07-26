aa7ca9f33f89d05f900c4a4adebc51e3484cb31b
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-26 17:52:43 +0530

    added changes for report generation by crtf pakcgae

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 9 : 9,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: process.env.CI ? 'dot' : 'list',
+  reporter: [['github-actions-ctrf', { outputFolder: 'playwright-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
