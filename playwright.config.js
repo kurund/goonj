336673defa7b64bf10b64a10f71688679d1ed69e
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-26 18:22:56 +0530

    added changes for report generation by playwright-ctrf-json-reporter pakcgae

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,9 @@ module.exports = defineConfig({
   workers: process.env.CI ? 9 : 9,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [['github-actions-ctrf', { outputFolder: 'playwright-report' }]],
+  reporter: [
+    ['playwright-ctrf-json-reporter', { outputFile: 'playwright-report.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
