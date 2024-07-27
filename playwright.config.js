29bc783b4029abaf28089f0d5ce93992ad60d704
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-28 00:25:34 +0530

    added changes for repor geneartion by github-actions-reportery

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 9 : 9,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
-  reporter: [['json', { outputFile: 'results.json' }]],
+  reporter: [['@estruyf/github-actions-reporter']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
