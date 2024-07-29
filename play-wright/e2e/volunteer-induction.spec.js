c97dc079e414ea1213270351936ca6dd859c1887
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-07-29 19:08:12 +0530

    updted test case with describe and before each code  added

--- play-wright/e2e/volunteer-induction.spec.js
+++ play-wright/e2e/volunteer-induction.spec.js
@@ -2,22 +2,30 @@ import { test, expect } from '@playwright/test';
 import { userDetails, userLogin, submitVolunteerRegistrationForm, searchAndVerifyContact  } from '../utils.js';
 import { VolunteerProfilePage } from '../pages/volunteer-profile.page';
 
-test('schedule induction and update induction status as completed', async ({ page }) => {
-  const volunteerProfilePage = new VolunteerProfilePage(page);
-  const contactType = 'Individual'
-  await submitVolunteerRegistrationForm(page, userDetails);
-  await page.waitForTimeout(2000)
-  await userLogin(page);
-  await searchAndVerifyContact(page, userDetails, contactType)
-  page.locator('a.view-contact').click({force: true})
-  await volunteerProfilePage.volunteerProfileTabs('activities');
-  await volunteerProfilePage.updateInductionForm('Induction', 'To be scheduled', 'Edit', 'Scheduled', 'save')
-  await volunteerProfilePage.updateInductionForm('Induction', 'Scheduled', 'Edit', 'Completed', 'save')
-  await page.click('a:has-text("Volunteers")');
-  await page.waitForTimeout(3000)
-  await volunteerProfilePage.clickVolunteerSuboption('Active')
-  await page.waitForTimeout(2000)
-  const activeVolunteerRowSelector = `table tbody tr:has(span[title="${userDetails.email}"])`;
-  await page.waitForTimeout(1000)
-  expect(activeVolunteerRowSelector).toContain(userDetails.email)
+test.describe('Volunteer Induction Tests', () => {
+  let volunteerProfilePage;
+  const contactType = 'Individual';
+
+  test.beforeEach(async ({ page }) => {
+    volunteerProfilePage = new VolunteerProfilePage(page);
+    await submitVolunteerRegistrationForm(page, userDetails);
+    await page.waitForTimeout(2000);
+    await userLogin(page);
+    await searchAndVerifyContact(page, userDetails, contactType);
+    await page.locator('a.view-contact').click({ force: true });
+  });
+
+  test('schedule induction and update induction status as completed', async ({ page }) => {
+    await volunteerProfilePage.volunteerProfileTabs('activities');
+    await volunteerProfilePage.updateInductionForm('Induction', 'To be scheduled', 'Edit', 'Scheduled', 'save')
+    await volunteerProfilePage.updateInductionForm('Induction', 'Scheduled', 'Edit', 'Completed', 'save')
+    await page.click('a:has-text("Volunteers")');
+    await page.waitForTimeout(3000)
+    await volunteerProfilePage.clickVolunteerSuboption('Active')
+    await page.waitForTimeout(2000)
+    const activeVolunteerRowSelector = `table tbody tr:has(span[title="${userDetails.email}"])`;
+    await page.waitForTimeout(2000)
+    expect(activeVolunteerRowSelector).toContain(userDetails.email)
+  });
+
 });
