6c75289bdc92c0e0d665921153e8635ea8738c7a
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-08-01 19:29:32 +0530

    enabled code to run in diffrent browsers

--- playwright.config.js
+++ playwright.config.js
@@ -47,16 +47,15 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
