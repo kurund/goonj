f430791b50ee0ce5ea45177cb276e4346ccce9c6
Tarun <tarun.joshi@coloredcow.com>
2024-08-02 16:46:18 +0530

    add the individual event to an activity

--- wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/CivirulesAction/CreateEventForContact.php
+++ wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/CivirulesAction/CreateEventForContact.php
@@ -7,26 +7,38 @@ class CRM_Goonjcustom_CivirulesAction_CreateEventForContact extends CRM_Civirule
 	 * @access public
 	 */
 	public function processAction(CRM_Civirules_TriggerData_TriggerData $triggerData) {
-		// Get the contact ID from the trigger data
 		$contactId = $triggerData->getContactId();
+		$originalData = $triggerData->getOriginalData();
 		
-		// Event ID to which contacts will be added.
-		$eventId = 39;
-		
-		$params = [
-			'event_id' => $eventId,
-			'contact_id' => $contactId,
-			'status_id' => 1 //Status ID for 'Attending'
+		// Extract fields
+		$startDate = isset($originalData['custom_73']) ? $originalData['custom_73'] : null;
+		$endDate = isset($originalData['custom_74']) ? $originalData['custom_74'] : null;
+	
+		$eventParams = [
+			'title' => 'Collection Camp Intent',
+			'event_type_id' => 7,
+			'start_date' => $startDate,
+			'end_date' => $endDate,
+			'is_active' => 1,
+			'is_public' => 1,
+			'default_role_id' => 1
 		];
-
-		// Call the CiviCRM API to create the event participant
+	
 		try {
-			$result = civicrm_api3('Participant', 'create', $params);
-			// Log success
-			error_log("Participant added to event. Result: " . print_r($result, TRUE));
+			// Create the event
+			$eventResult = civicrm_api3('Event', 'create', $eventParams);
+			$newEventId = $eventResult['id'];
+	
+			// Add the participant to the newly created event
+			$participantParams = [
+				'event_id' => $newEventId,
+				'contact_id' => $contactId,
+				'status_id' => 1
+			];
+	
+			civicrm_api3('Participant', 'create', $participantParams);
 		} catch (CiviCRM_API3_Exception $e) {
-			// Log error
-			error_log("Error adding participant: " . $e->getMessage());
+			throw new Exception($e->getMessage());
 		}
 	}
 
