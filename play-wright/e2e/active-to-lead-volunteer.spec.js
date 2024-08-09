949c70ec826aa9a3981db98e9e5fae891d961737
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2024-08-09 19:53:03 +0530

    updated test case with email stored in lower case

--- play-wright/e2e/active-to-lead-volunteer.spec.js
+++ play-wright/e2e/active-to-lead-volunteer.spec.js
@@ -6,6 +6,7 @@ test('Add a volunteer to Lead Volunteer group', async ({ page }) => {
   const volunteerProfilePage = new VolunteerProfilePage(page);
   const individualContactType = 'Individual'
   const volunteerContactType = '- Volunteer'
+  let userEmailAddress = userDetails.email.toLowerCase()
   await submitVolunteerRegistrationForm(page, userDetails);
   await page.waitForTimeout(2000)
   await userLogin(page);
@@ -18,14 +19,14 @@ test('Add a volunteer to Lead Volunteer group', async ({ page }) => {
   await page.waitForTimeout(3000)
   await volunteerProfilePage.clickVolunteerSuboption('Active')
   await page.waitForTimeout(8000)
-  await page.fill('#contact-email-contact-id-01-email-0', userDetails.email)
+  await page.fill('#contact-email-contact-id-01-email-0', userEmailAddress)
   await page.press('#contact-email-contact-id-01-email-0', 'Enter')
   await page.waitForTimeout(2000)
   const emailSelector = 'td[data-field-name=""] span.ng-binding.ng-scope';
   const emailAddress = await page.$$eval(emailSelector, nodes =>
     nodes.map(n => n.innerText.trim())
   );
-  const userEmailAddress = userDetails.email.toLowerCase()
+  // const userEmailAddress = userDetails.email.toLowerCase()
   expect(emailAddress).toContain(userEmailAddress)
   await searchAndVerifyContact(page, userDetails, volunteerContactType)
   await volunteerProfilePage.volunteerProfileTabs('groups');
@@ -36,7 +37,7 @@ test('Add a volunteer to Lead Volunteer group', async ({ page }) => {
   await page.click('table#option11 a:has-text("Lead Volunteers")');
   await page.click('#searchForm summary:has-text("Find Contacts within this Group")');
   await searchAndVerifyContact(page, userDetails, volunteerContactType)
-  const leadVolunteerRowSelector = `table tbody tr:has(span[title="${userDetails.email}"])`;
+  const leadVolunteerRowSelector = `table tbody tr:has(span[title="${userEmailAddress}"])`;
   await page.waitForTimeout(1000)
-  expect(leadVolunteerRowSelector).toContain(userDetails.email)
+  expect(leadVolunteerRowSelector).toContain(userEmailAddress)
 });
