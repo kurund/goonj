b4f9800707652882c84c19266672cd4edf8fae8b
nishant22029 <nishant.kumar@coloredcow.in>
2024-08-12 15:31:44 +0530

    added change to send email with induction details

--- wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/Cron.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/Cron.php
@@ -29,7 +29,7 @@ function civicrm_api3_goonjcustom_cron($params) {
 
     // $currentDate = date('Y-m-d');
     $activity_assignees = \Civi\Api4\Activity::get(TRUE)
-        ->addSelect('activity_contact.contact_id')
+        ->addSelect('target_contact_id', 'activity_contact.contact_id', 'activity_date_time')
         ->addJoin('ActivityContact AS activity_contact', 'LEFT', ['activity_contact.record_type_id', '=', 1])
         ->addWhere('activity_type_id', '=', 57)
         ->addWhere('status_id', '=', 1)
@@ -40,13 +40,50 @@ function civicrm_api3_goonjcustom_cron($params) {
 
 
     foreach ($activity_assignees as $assignee) {
-        $contact_id = $assignee['activity_contact.contact_id'];
-        $contactEmail = \Civi\Api4\Email::get(TRUE)
-            ->addSelect('contact_id', 'email')
-            ->addWhere('contact_id', '=', $contact_id)
+        $assignee_contact_id = $assignee['activity_contact.contact_id'];
+        $volunteer_contect_id = $assignee['target_contact_id'][0];
+        error_log('$result' . print_r($assignee['target_contact_id'][0], true));
+        $assignee_details = \Civi\Api4\Contact::get(TRUE)
+            ->addSelect('email.email', 'display_name')
+            ->addJoin('Email AS email', 'LEFT')
+            ->addWhere('id', '=', $assignee_contact_id)
             ->execute();
-        error_log('contactEmail' . print_r($contactEmail, true));
-
+        error_log('$result2' . print_r($assignee_details, true));
+        $volunteer_details = \Civi\Api4\Contact::get(TRUE)
+            ->addSelect('display_name')
+            ->addWhere('id', '=', $volunteer_contect_id)
+            ->execute();
+        error_log('$result3' . print_r($volunteer_details, true));
+        list($defaultFromName, $defaultFromEmail) = CRM_Core_BAO_Domain::getNameAndEmail();
+        $from = "\"$defaultFromName\" <$defaultFromEmail>";
+        $assignee_name = $assignee_details[0]['display_name'];
+        $volunteer_name = $volunteer_details[0]['display_name'];
+    
+        $induction_time = $assignee['activity_date_time'];
+        $html = "
+        <p>Dear $assignee_name,</p>
+        <p>This is a friendly reminder that you have a volunteer induction scheduled for today at $induction_time.</p>
+        <p>Here are the details for the volunteer being inducted:</p>
+        <ul>
+            <li><strong>Name:</strong> $volunteer_name</li>
+        </ul>
+        <p>Best regards,<br>Goonj</p>";
+        $mailParams = [
+            'groupName' => 'Mailing Event ',
+            'subject' => 'Reminder: Volunteer Induction Scheduled for Today',
+            'from' => $from,
+            'toEmail' => $assignee_details[0]['email.email'],
+            'toName' => $assignee_details[0]['display_name'],
+            'replyTo' => $from,
+            'html' => $html,
+            // 'messageTemplateID' => 76,
+          ];
+        try{
+            $result = CRM_Utils_Mail::send($mailParams);
+            error_log('$result' . print_r($result, true));
+        }catch (CiviCRM_API3_Exception $e) {
+            error_log('Goonj Cron Job: API error - ' . $e->getMessage());
+        }
     }
     
     error_log('Goonj Cron Job: Job completed successfully');
