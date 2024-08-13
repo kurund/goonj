7bcabf85b53499259927367c6cca919a96aca5f3
nishant22029 <nishant.kumar@coloredcow.in>
2024-08-13 13:48:03 +0530

    added changes to send multiple volunteer details

--- wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/Cron.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/Cron.php
@@ -29,7 +29,6 @@ function civicrm_api3_goonjcustom_cron($params)
 
     try {
 
-        $currentDate = date('Y-m-d');
         $induction_activity_type_id = 57;
         $assignee_record_type_id = 1 ;
         $induction_status_id = 1 ;
@@ -47,47 +46,108 @@ function civicrm_api3_goonjcustom_cron($params)
             ->setLimit(25)
             ->execute();
 
+        $groupedResults = [];
 
-        foreach ($activity_assignees as $assignee) {
-            $assignee_contact_id = $assignee['activity_contact.contact_id'];
-            $volunteer_contact_id = $assignee['target_contact_id'][0];
+        // Initialize an array to hold the grouped results
+        $groupedResults = [];
 
+        foreach ($activity_assignees as $activity) {
+            $assignee_contact_id = $activity['activity_contact.contact_id'];
+            $targetContactId = $activity['target_contact_id'][0];
+            $activityDateTime = $activity['activity_date_time'];
+
+
+            // Get details for the assignee contact
             $assignee_details = \Civi\Api4\Contact::get(true)
                 ->addSelect('email.email', 'display_name')
                 ->addJoin('Email AS email', 'LEFT')
                 ->addWhere('id', '=', $assignee_contact_id)
                 ->execute();
 
+            // Get details for the target contact
             $volunteer_details = \Civi\Api4\Contact::get(true)
-                ->addSelect('display_name')
-                ->addWhere('id', '=', $volunteer_contact_id)
+                ->addSelect('email.email', 'phone.phone', 'display_name')
+                ->addJoin('Email AS email', 'LEFT')
+                ->addJoin('Phone AS phone', 'LEFT')
+                ->addWhere('id', '=', $targetContactId)
+                ->setLimit(25)
                 ->execute();
 
+            // If the contact_id is not in the grouped results, initialize it
+            if (!isset($groupedResults[$assignee_contact_id])) {
+                $groupedResults[$assignee_contact_id] = [
+                    'activity_contact.contact_id' => $assignee_contact_id,
+                    'assignee_display_name' => $assignee_details[0]['display_name'],
+                    'assignee_email' => $assignee_details[0]['email.email'],
+                    'target_contact_details' => [],
+                ];
+            }
+
+            $groupedResults[$assignee_contact_id]['target_contact_details'][] = [
+                'id' => $targetContactId,
+                'activity_date_time' => $activityDateTime,
+                "volunteer_display_name" => $volunteer_details[0]['display_name'],
+                'email' => $volunteer_details[0]['email.email'] ?? '',
+                'phone' => $volunteer_details[0]['phone.phone'] ?? '',
+            ];
+        }
+
+        // Convert the grouped results to a list of arrays
+        $finalResults = array_values($groupedResults);
+
+
+
+        foreach ($finalResults as $assignee) {
+            $assignee_contact_id = $assignee['activity_contact.contact_id'];
+            $assignee_name = $assignee['assignee_display_name'];
+
+            $assignee_email = $assignee['assignee_email'];
+
+            // Prepare the email body content
+            $volunteer_details_html = '';
+            foreach ($assignee['target_contact_details'] as $target) {
+                $volunteer_id = $target['id'];
+                $volunteer_name = \Civi\Api4\Contact::get(true)
+                    ->addSelect('display_name')
+                    ->addWhere('id', '=', $volunteer_id)
+                    ->execute()[0]['display_name'];
+
+                $activity_date_time = $target['activity_date_time'];
+                $formatted_date_time = new DateTime($activity_date_time);
+                $induction_time = $formatted_date_time->format('F jS, Y g:i A');
+
+                // Append each volunteer's details to the email body
+                $volunteer_details_html .= "
+                <li><strong>Name:</strong> $volunteer_name<br>
+                    <strong>Email:</strong> {$target['email']}<br>
+                    <strong>Phone:</strong> {$target['phone']}<br>
+                    <strong>Scheduled At:</strong> $induction_time
+                </li>";
+            }
+
+            $html = "
+            <p>Dear $assignee_name,</p>
+            <p>This is a friendly reminder that you have volunteer inductions scheduled for today.</p>
+            <p>Here are the details for the volunteers being inducted:</p>
+            <ul>
+                $volunteer_details_html
+            </ul>
+            <p>Best regards,<br>Goonj</p>";
 
             list($defaultFromName, $defaultFromEmail) = CRM_Core_BAO_Domain::getNameAndEmail();
             $from = "\"$defaultFromName\" <$defaultFromEmail>";
-            $assignee_name = $assignee_details[0]['display_name'];
-            $volunteer_name = $volunteer_details[0]['display_name'];
 
-            $induction_time = $assignee['activity_date_time'];
-            $html = "
-        <p>Dear $assignee_name,</p>
-        <p>This is a friendly reminder that you have a volunteer induction scheduled for today at $induction_time.</p>
-        <p>Here are the details for the volunteer being inducted:</p>
-        <ul>
-            <li><strong>Name:</strong> $volunteer_name</li>
-        </ul>
-        <p>Best regards,<br>Goonj</p>";
             $mailParams = [
-                'groupName' => 'Mailing Event ',
-                'subject' => 'Reminder: Volunteer Induction Scheduled for Today',
+                'groupName' => 'Mailing Event',
+                'subject' => 'Reminder: Volunteer Inductions Scheduled for Today',
                 'from' => $from,
-                'toEmail' => $assignee_details[0]['email.email'],
-                'toName' => $assignee_details[0]['display_name'],
+                'toEmail' => $assignee_email,
+                'toName' => $assignee_name,
                 'replyTo' => $from,
                 'html' => $html,
-                // 'messageTemplateID' => 76,
-              ];
+                // 'messageTemplateID' => 76, // Uncomment if using a message template
+            ];
+
             try {
                 $result = CRM_Utils_Mail::send($mailParams);
             } catch (CiviCRM_API3_Exception $e) {
