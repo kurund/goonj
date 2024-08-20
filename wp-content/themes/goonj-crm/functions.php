d78c41936eb7c5bc5d743124d8a25fd151d3015a
belwalshubham <belwalshubham08@gmail.com>
2024-08-20 22:59:22 +0530

    fix(*): Implement Dynamic Field Visibility Based on Select2 Selection

--- wp-content/themes/goonj-crm/functions.php
+++ wp-content/themes/goonj-crm/functions.php
@@ -34,6 +34,9 @@ function goonj_enqueue_admin_scripts() {
 }
 
 
+wp_enqueue_script('custom-script', get_template_directory_uri() . '/custom-script.js', array(), null, true);
+
+
 add_action('after_setup_theme', 'goonj_theme_setup');
 function goonj_theme_setup() {
 	add_editor_style('style.css');
