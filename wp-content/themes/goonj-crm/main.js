94102f631c2068641d84dbc72361316d3959543b
belwalshubham <belwalshubham08@gmail.com>
2024-08-27 16:24:58 +0530

    fix(*): code cleanups

--- wp-content/themes/goonj-crm/main.js
+++ wp-content/themes/goonj-crm/main.js
@@ -38,74 +38,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 });
 
-// This script resets form fields when the reset button is clicked.
+/* 
+Note: This page refresh mechanism is implemented as a temporary solution 
+to reset all fields when the reset button is clicked. It will be replaced 
+with a more effective solution once identified.
+*/
 document.addEventListener("DOMContentLoaded", function () {
-	setTimeout(function () {
-	  var container = document.querySelector("#bootstrap-theme > crm-angular-js");
-  
-	  var resetButton = container
-		? container.querySelector('button[type="reset"]')
-		: null;
-  
-	  if (container && resetButton) {
-		resetButton.addEventListener("click", function (event) {
-		  event.preventDefault();
-  
-		  var angularElement = angular.element(container);
-		  var scope = angularElement.scope();
-		  if (scope) {
-			var fields = container.querySelectorAll(
-			  "input, textarea, select, ul.select2-choices, div.select2-container"
-			);
-  
-			fields.forEach(function (field) {
-			  if (
-				field.tagName.toLowerCase() === "ul" &&
-				field.classList.contains("select2-choices")
-			  ) {
-				var selectedItems = field.querySelectorAll(
-				  ".select2-search-choice"
-				);
-				selectedItems.forEach(function (item) {
-				  item.remove();
-				});
-			  } else if (
-				field.tagName.toLowerCase() === "div" &&
-				field.classList.contains("select2-container")
-			  ) {
-				var chosenSpan = field.querySelector(".select2-chosen");
-				var select2Choice = field.querySelector(".select2-choice");
-  
-				if (chosenSpan) {
-				  chosenSpan.textContent = "Select";
-				}
-				if (select2Choice) {
-				  select2Choice.classList.add("select2-default");
-				}
-				if (field.classList.contains("select2-allowclear")) {
-				  field.classList.remove("select2-allowclear");
-				}
-			  } else if (
-				field.tagName.toLowerCase() === "input" ||
-				field.tagName.toLowerCase() === "textarea"
-			  ) {
-				field.value = "";
-			  } else if (field.tagName.toLowerCase() === "select") {
-				field.selectedIndex = 0;
-			  }
-			});
-  
-			scope.$apply();
-			/* 
-			Note: This reload mechanism is implemented as a temporary measure to prevent form submission with old data after a reset. 
-			It will be replaced with a more appropriate solution in the future.
-			*/
-			setTimeout(function () {
-			  location.reload(true);
-			}, 100);
-		  }
-		});
-	  }
-	}, 1000);
-  });
-  
+  setTimeout(function () {
+    var resetButton = document.querySelector('button[type="reset"]');
+
+    if (resetButton) {
+      resetButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        // Refresh the page to reset all fields
+        location.reload(true);
+      });
+    }
+  }, 1000);
+});
