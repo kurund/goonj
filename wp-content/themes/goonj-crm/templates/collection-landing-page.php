7a6e30f4204e563a3a32fd791d9dcb40a27e8a02
pokhiii <pokhi@coloredcow.com>
2024-08-27 17:08:28 +0530

    update link

--- wp-content/themes/goonj-crm/templates/collection-landing-page.php
+++ wp-content/themes/goonj-crm/templates/collection-landing-page.php
@@ -2,29 +2,23 @@
 /*
 Template Name: Collection Landing Page
 */
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    wp_redirect(get_home_url() . "/collection-camp");
-    exit;
-}
 ?>
+
 <div class="text-center ml-30 mr-30">
-    <h2 class="font-sans fz-20 fw-400 mb-6">Goonj Collection Camp</h2>
-    <p class="font-sans fz-16 fw-400 mt-0 mb-24">Please go through the details below</p>
-    <ol>
-        <li class="font-sans mb-6 fz-16">
-            Before organising the collection drives, kindly go through our guidelines here - <a href="https://rb.gy/9nfbus" target="_blank">https://rb.gy/9nfbus</a>. Understanding all the guidelines beforehand is important to avoid any inconvenience later.
-        </li>
-        <li class="font-sans mb-6 fz-16">
-            Collection drives can be organised in the cities where Goonj has its offices after proper coordination with the relevant offices - Delhi, Rishikesh, Mumbai, Chennai, Bangalore, Hyderabad, Kochi and Kolkata.
-        </li>
-        <li class="font-sans mb-6 fz-16">
-            If you are in any other city besides the cities mentioned above, we would request you to facilitate the crucial aspect of reaching the collected material to the nearest Goonj office. For list of Goonj offices, please refer - <a href="https://goonj.org/our-offices/" target="_blank">https://goonj.org/our-offices/</a>
-        </li>
-    </ol>
-    <form method="post">
-        <p class="login-submit">
-            <input type="submit" class="button button-primary w-100p mt-36" value="Continue">
-        </p>
-    </form>
+	<h2 class="font-sans fz-20 fw-400 mb-6">Goonj Collection Camp</h2>
+	<p class="font-sans fz-16 fw-400 mt-0 mb-24">Please go through the details below</p>
+	<ol>
+		<li class="font-sans mb-6 fz-16">
+			Before organising the collection drives, kindly go through our guidelines here - <a href="https://rb.gy/9nfbus" target="_blank">https://rb.gy/9nfbus</a>. Understanding all the guidelines beforehand is important to avoid any inconvenience later.
+		</li>
+		<li class="font-sans mb-6 fz-16">
+			Collection drives can be organised in the cities where Goonj has its offices after proper coordination with the relevant offices - Delhi, Rishikesh, Mumbai, Chennai, Bangalore, Hyderabad, Kochi and Kolkata.
+		</li>
+		<li class="font-sans mb-6 fz-16">
+			If you are in any other city besides the cities mentioned above, we would request you to facilitate the crucial aspect of reaching the collected material to the nearest Goonj office. For list of Goonj offices, please refer - <a href="https://goonj.org/our-offices/" target="_blank">https://goonj.org/our-offices/</a>
+		</li>
+	</ol>
+	<div class="login-submit w-100p mt-36">
+		<a class="button button-primary" href="<?php echo esc_url( get_page_by_path( 'collection-camp/check-user' ) ); ?>">Continue</a>
+	</div>
 </div>
