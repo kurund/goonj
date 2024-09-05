dc7b6574fa17ba0a9b8d80b388ef5654c71c94d8
pokhiii <pokhi@coloredcow.com>
2024-09-06 02:12:17 +0530

    remove redudant code

--- wp-content/civi-extensions/goonjcustom/goonjcustom.php
+++ wp-content/civi-extensions/goonjcustom/goonjcustom.php
@@ -6,6 +6,7 @@
 
 require_once 'goonjcustom.civix.php';
 
+use Civi\Api4\Address;
 use Civi\Api4\Contact;
 use Civi\Token\Event\TokenRegisterEvent;
 use Civi\Token\Event\TokenValueEvent;
@@ -68,208 +69,61 @@ function goonjcustom_register_tokens(TokenRegisterEvent $e) {
  *
  */
 function goonjcustom_evaluate_tokens(TokenValueEvent $e) {
-	foreach ($e->getRows() as $row) {
-	  /** @var TokenRow $row */
-	  $row->format('text/html');
-  
-	  $contactId = $row->context['contactId'];
-  
-	  if (empty($contactId)) {
-		$row->tokens('contact', 'inductionDetails', '');
-		continue;
-	  }
-  
-	  $contacts = \Civi\Api4\Contact::get(FALSE)
-	  ->addSelect('address_primary.state_province_id')
-	  ->addWhere('id', '=', $contactId)
-	  ->execute();
-  
-	  $statedata = $contacts->first();
-	  $stateId = $statedata['address_primary.state_province_id'];
-  
-	  $processingCenters = \Civi\Api4\Contact::get(FALSE)
-	  ->addSelect('Goonj_Office_Details.Days_for_Induction', '*', 'custom.*', 'addressee_id', 'id')
-	  ->addWhere('contact_sub_type', 'CONTAINS', 'Goonj_Office')
-	  ->addWhere('contact_type', '=', 'Organization')
-	  ->addWhere('Goonj_Office_Details.Induction_Catchment', 'CONTAINS', $stateId)
-	  ->execute();
-  
-	  $inductionDetailsMarkup = 'The next step in your volunteering journey is to get inducted with Goonj.';
-  
-	  If ($processingCenters->rowCount > 0) {
-	  if (TRUE) {
-		  $inductionDetailsMarkup .= ' You can visit any of our following center(s) during the time specified to complete your induction:';
-		  $inductionDetailsMarkup .= '<ol>';
-  
-		  foreach ($processingCenters as $processingCenter) {
-			  $centerID = $processingCenter['id'];
-			  // Fetch the primary address for the current center
-			  $addressesData = \Civi\Api4\Address::get(false)
-			  ->addWhere('contact_id', '=', $centerID)
-			  ->addWhere('is_primary', '=', true)
-			  ->setLimit(1)
-			  ->execute();
-			  $address = $addressesData->first();
-			  $contactAddress = $address['street_address'];
-  
-			  $inductionDetailsMarkup .= '<li><strong>' . $processingCenter['organization_name'] . '</strong><br /><span style="margin-top: 10px; display: block;">' . $contactAddress . '</span> ' . $processingCenter['Goonj_Office_Details.Days_for_Induction'] . '</li>';
-  
-		  }
-  
-		  $inductionDetailsMarkup .= '</ol>';
-	  }
-	  else {
-		  $inductionDetailsMarkup .= ' Unfortunately, we don\'t currently have a processing center near to the location you have provided. Someone from our team will reach out and will share the details of induction.';
-	  }
-  
-	  $row->tokens('contact', 'inductionDetails', $inductionDetailsMarkup);
-	  }
-	  }
-  }
+  foreach ($e->getRows() as $row) {
+    /** @var TokenRow $row */
+    $row->format('text/html');
 
-/**
- * Implements hook_civicrm_buildForm().
- *
- * Set a default value for an event price set field.
- *
- * @param string $formName
- * @param CRM_Core_Form $form
- */
-function goonjcustom_civicrm_buildForm($formName, $form) {
-  if ($formName == 'CRM_Activity_Form_Activity') {
-    $activityTypeId = $form->getVar('_activityTypeId');
-    // @todo find better way than hardcoding
-    if ($activityTypeId === 57) {
-        $fieldsToRemove = [
-        'subject',
-        'engagement_level',
-        'location',
-        'duration',
-        'priority_id',
-        ];
+    $contactId = $row->context['contactId'];
 
-        foreach ($fieldsToRemove as $field) {
-        if (isset($form->_elementIndex[$field])) {
-            $form->removeElement($field);
-        }
+    if (empty($contactId)) {
+      $row->tokens('contact', 'inductionDetails', '');
+      continue;
+    }
+
+    $contacts = Contact::get(FALSE)
+      ->addSelect('address_primary.state_province_id')
+      ->addWhere('id', '=', $contactId)
+      ->execute();
+
+    $statedata = $contacts->first();
+    $stateId = $statedata['address_primary.state_province_id'];
+
+    $processingCenters = Contact::get(FALSE)
+      ->addSelect('Goonj_Office_Details.Days_for_Induction', '*', 'custom.*', 'addressee_id', 'id')
+      ->addWhere('contact_sub_type', 'CONTAINS', 'Goonj_Office')
+      ->addWhere('contact_type', '=', 'Organization')
+      ->addWhere('Goonj_Office_Details.Induction_Catchment', 'CONTAINS', $stateId)
+      ->execute();
+
+    $inductionDetailsMarkup = 'The next step in your volunteering journey is to get inducted with Goonj.';
+
+    if ($processingCenters->rowCount > 0) {
+      if (TRUE) {
+        $inductionDetailsMarkup .= ' You can visit any of our following center(s) during the time specified to complete your induction:';
+        $inductionDetailsMarkup .= '<ol>';
+
+        foreach ($processingCenters as $processingCenter) {
+          $centerID = $processingCenter['id'];
+          // Fetch the primary address for the current center.
+          $addressesData = Address::get(FALSE)
+            ->addWhere('contact_id', '=', $centerID)
+            ->addWhere('is_primary', '=', TRUE)
+            ->setLimit(1)
+            ->execute();
+          $address = $addressesData->first();
+          $contactAddress = $address['street_address'];
+
+          $inductionDetailsMarkup .= '<li><strong>' . $processingCenter['organization_name'] . '</strong><br /><span style="margin-top: 10px; display: block;">' . $contactAddress . '</span> ' . $processingCenter['Goonj_Office_Details.Days_for_Induction'] . '</li>';
+
         }
 
-        CRM_Core_Region::instance('page-body')->add([
-		'script' => "
-					CRM.$(function($) {
-						function updateCustomGroupVisibility() {
-							var selectedText = $('#status_id').find('option:selected').text();
-							var customGroup = $('.custom-group-INDUCTION_DETAILS');
+        $inductionDetailsMarkup .= '</ol>';
+      }
+      else {
+        $inductionDetailsMarkup .= ' Unfortunately, we don\'t currently have a processing center near to the location you have provided. Someone from our team will reach out and will share the details of induction.';
+      }
 
-							if (selectedText === 'Completed') {
-								customGroup.show();
-								// TODO - need to add logic to handle required fields
-							} else {
-								customGroup.hide();
-								// TODO - need to add logic to handle required fields
-							}
-						}
-						$('#status_id').change(function() {
-							updateCustomGroupVisibility();
-						});
-
-						// It takes time to load the form fields so adding a delay to run the js once form is loaded.
-						setTimeout(updateCustomGroupVisibility, 500);
-					});
-				",
-	  ]);
-	}
+      $row->tokens('contact', 'inductionDetails', $inductionDetailsMarkup);
+    }
   }
-}
-
-/**
- *
- */
-function goonjcustom_civicrm_pageRun(&$page) {
-  // Check if we are on the activity edit form page.
-  CRM_Core_Region::instance('page-footer')->add(
-		[
-		  'script' => '
-			  (function($) {
-				  $(document).ajaxComplete(function(event, xhr, settings) {
-					var isInductionActivity = false;
-					var isCollectionCampActivity = false;
-					var isActivityViewType61 = false;
-					var urlParams = new URLSearchParams(settings.url);
-					var activityTypeId57 = "57";
-					var activityTypeId61 = "61";
-
-					if ((urlParams.get("atype") === activityTypeId57 && urlParams.get("action") === "view") ||
-						(urlParams.get("subType") === activityTypeId57 && urlParams.get("action") === "2")) {
-						isInductionActivity = true;
-					}
-
-					if ((urlParams.get("atype") === activityTypeId61 && urlParams.get("action") === "view") ||
-						(urlParams.get("subType") === activityTypeId61 && urlParams.get("action") === "2")) {
-						isCollectionCampActivity = true;
-					}
-
-					if (urlParams.get("subType") === activityTypeId61 && urlParams.get("action") === "view") {
-						isActivityViewType61 = true;
-					}
-
-					if (isInductionActivity || isCollectionCampActivity || isActivityViewType61) {
-						var fieldsToHide = [];
-
-						if (isInductionActivity) {
-							fieldsToHide = [
-								".crm-activity-form-block-subject",
-								".crm-activity-form-block-campaign_id",
-								".crm-activity-form-block-engagement_level",
-								".crm-activity-form-block-duration",
-								".crm-activity-form-block-priority_id",
-								".crm-activity-form-block-location",
-								".crm-activity-form-block-attachment",
-								".crm-activity-form-block-recurring_activity",
-								".crm-activity-form-block-schedule_followup",
-							];
-						} else if (isCollectionCampActivity) {
-							fieldsToHide = [
-								".crm-activity-form-block-subject",
-								".crm-activity-form-block-engagement_level",
-								".crm-activity-form-block-duration",
-								".crm-activity-form-block-priority_id",
-								".crm-activity-form-block-location",
-								".crm-activity-form-block-attachment",
-								".crm-activity-form-block-recurring_activity",
-								".crm-activity-form-block-schedule_followup",
-								".crm-activity-form-block-target_contact_id",
-								".crm-activity-form-block-assignee_contact_id",
-								".crm-activity-form-block-activity_date_time",
-								".crm-activity-form-block-details",
-							];
-						} else if (isActivityViewType61) {
-							fieldsToHide = [
-								".crm-activity-form-block-target_contact_id",
-								".crm-activity-form-block-assignee_contact_id",
-								".crm-activity-form-block-engagement_level",
-								".crm-activity-form-block-duration",
-								".crm-activity-form-block-details",
-								".crm-activity-form-block-priority_id",
-								".crm-activity-form-block-subject",
-								".crm-activity-form-block-location",
-								".crm-activity-form-block-campaign_id",
-							];
-						}
-
-						fieldsToHide.forEach(function(field) {
-							$(field).css("display", "none");
-						});
-
-						if (isInductionActivity) {
-							var inductionFields = $(".custom-group-Induction_Fields tr");
-							$(".crm-activity-form-block-activity_date_time").after(inductionFields);
-							$(".custom-group-Induction_Fields").remove();
-						}
-					}
-					});
-				})(CRM.$);
-			',
-		]
-  );
 }
