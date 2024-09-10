ce7732e1cfc7f07a81a94f2cd201b5ddd37803e0
pokhiii <pokhi@coloredcow.com>
2024-09-10 14:24:52 +0530

    wip acl collection camp

--- wp-content/civi-extensions/goonjcustom/Civi/CollectionBaseService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/CollectionBaseService.php
@@ -2,6 +2,8 @@
 
 namespace Civi;
 
+use Civi\Api4\Group;
+use Civi\Api4\GroupContact;
 use Civi\Core\Service\AutoSubscriber;
 
 /**
@@ -17,6 +19,7 @@ class CollectionBaseService extends AutoSubscriber {
   public static function getSubscribedEvents() {
     return [
       '&hook_civicrm_tabset' => 'collectionBaseTabset',
+      '&hook_civicrm_selectWhereClause' => 'aclCollectionCamp',
     ];
   }
 
@@ -41,7 +44,58 @@ class CollectionBaseService extends AutoSubscriber {
       'active' => 1,
       'current' => FALSE,
     ];
+  }
 
+  /**
+   *
+   */
+  public static function aclCollectionCamp($entity, &$clauses, $userId, $conditions) {
+    if ($entity !== 'Eck_Collection_Camp') {
+      return;
+    }
+
+    $teamGroupContacts = GroupContact::get(FALSE)
+      ->addSelect('group_id')
+      ->addWhere('contact_id', '=', $userId)
+      ->addWhere('status', '=', 'Added')
+      ->addWhere('group_id.Chapter_Contact_Group.Use_Case', '=', 'chapter-team')
+      ->execute();
+
+    $teamGroupContact = $teamGroupContacts->first();
+
+    if (!$teamGroupContact) {
+      \Civi::log()->debug('no chapter team group found for user: ' . $userId);
+
+      return;
+    }
+
+    $groupId = $teamGroupContact['group_id'];
+
+    $chapterGroups = Group::get(FALSE)
+      ->addSelect('Chapter_Contact_Group.States_controlled')
+      ->addWhere('id', '=', $groupId)
+      ->execute();
+
+    $group = $chapterGroups->first();
+    $statesControlled = $group['Chapter_Contact_Group.States_controlled'];
+
+    if (!empty($statesControlled)) {
+      $statesControlled = array_unique($statesControlled);
+      $statesList = implode(',', array_map('intval', $statesControlled));
+
+      $clauses['custom_261'][] = "IN ($statesList)";
+    }
+    else {
+      $clauses['custom_261'][] = "IN (NULL)";
+    }
+
+    \Civi::log()->debug($entity, [
+      [
+        'groupId' => $groupId,
+        'statesControlled' => $statesControlled,
+        'statesList' => $statesList,
+      ],
+    ]);
   }
 
 }
