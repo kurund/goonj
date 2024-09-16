f89a8cc6b1b04dd73e7a78b460370879210bc77b
Tarun <tarun.joshi@coloredcow.com>
2024-09-16 16:22:16 +0530

    remove logs

--- wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustomevent/Cron.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustomevent/Cron.php
@@ -62,7 +62,6 @@ function goonjcustomevent_check_and_send_emails_for_camp_end_date() {
     $today = new DateTime();
     $todayFormatted = $today->format('Y-m-d');
 
-    // Iterate through the collection camps.
     foreach ($collectionCamps as $camp) {
       if (empty($camp['Logistics_Coordination.Camp_to_be_attended_by'])) {
         continue;
@@ -71,17 +70,9 @@ function goonjcustomevent_check_and_send_emails_for_camp_end_date() {
       $endDate = new DateTime($camp['Collection_Camp_Intent_Details.End_Date']);
       $endDateFormatted = $endDate->format('Y-m-d');
 
-      // Debug logs to verify values.
-      error_log("recipientId: " . print_r($recipientId, TRUE));
-      error_log("endDate: " . print_r($endDate, TRUE));
-      error_log("endDateFormatted: " . print_r($endDateFormatted, TRUE));
-      // Log today's date.
-      error_log("todayFormatted: " . print_r($todayFormatted, TRUE));
-
       // Only send the email if the end date is exactly today.
       if ($endDateFormatted === $todayFormatted) {
         emailToCampAttendBy($recipientId, $endDateFormatted);
-        error_log("Email sent to participant with ID: " . $recipientId);
       }
     }
   }
@@ -101,21 +92,16 @@ function emailToCampAttendBy($contactId, $collectionCampEndDate) {
     ->execute();
 
   $messageTemplateId = $messageTemplates->first()['id'];
-  error_log('messageTemplateId ' . print_r($messageTemplateId, TRUE));
 
   // Prepare email parameters.
   $emailParams = [
     'contact_id'  => $contactId,
-  // Replace with your actual template ID.
     'template_id' => $messageTemplateId,
   ];
-  error_log('emailParams ' . print_r($emailParams, TRUE));
 
   // Send the email using CiviCRM API.
   try {
     $result = civicrm_api3('Email', 'send', $emailParams);
-    error_log('result ' . print_r($result, TRUE));
-
     if (isset($result['is_error']) && $result['is_error']) {
       error_log('Email failed to send: ' . $result['error_message']);
     }
