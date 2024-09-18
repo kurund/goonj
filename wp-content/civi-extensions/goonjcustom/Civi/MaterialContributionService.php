501f1d763d5816038272dd79e0aaf7dd998834a1
belwalshubham <belwalshubham08@gmail.com>
2024-09-18 11:00:24 +0530

    fix(*): pdf attachment

--- wp-content/civi-extensions/goonjcustom/Civi/MaterialContributionService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/MaterialContributionService.php
@@ -48,11 +48,46 @@ class MaterialContributionService extends AutoSubscriber {
 
     $contribution = $activities->first();
 
+    $contactData = civicrm_api4('Contact', 'get', [
+      'select' => [
+        'email_primary.email',
+        'phone_primary.phone',
+        'address_primary.street_address',
+      ],
+      'where' => [
+        ['id', '=', $params['contactId']],
+      ],
+      'limit' => 1,
+      'checkPermissions' => TRUE,
+    ]);
+    
+    
+    // Fetch data from the Civi\Api4\Generic\Result object using the toArray() method
+    $contactDataArray = $contactData[0];
+    
+    error_log("contactDataArray: " . print_r($contactDataArray, TRUE));// Access the first result directly.
+    
+    if (!empty($contactDataArray)) {
+      // Directly access the fields in $contactDataArray
+      $email = $contactDataArray['email_primary.email'] ?? 'N/A';
+      $phone = $contactDataArray['phone_primary.phone'] ?? 'N/A';
+      $address = $contactDataArray['address_primary.street_address'] ?? 'N/A';
+      
+      error_log("Contact Info - Email: $email, Phone: $phone");
+  } else {
+      // Log error or handle the case when contact data is not found
+      error_log("No contact data found for contactId: " . $params['contactId']);
+      $email = 'N/A';
+      $phone = 'N/A';
+  }
+    
+    
+
     if (!$contribution) {
       return;
     }
 
-    $html = self::generateContributionReceiptHtml($contribution);
+    $html = self::generateContributionReceiptHtml($contribution, $email, $phone, $address);
     $fileName = 'material_contribution_' . $contribution['id'] . '.pdf';
     $params['attachments'][] = \CRM_Utils_Mail::appendPDF($fileName, $html);
   }
@@ -66,17 +101,20 @@ class MaterialContributionService extends AutoSubscriber {
    * @return string
    *   The generated HTML.
    */
-  private static function generateContributionReceiptHtml($activity) {
+  private static function generateContributionReceiptHtml($activity, $email, $phone, $address) {
+
     // Format the activity date.
     $activityDate = date("F j, Y", strtotime($activity['activity_date_time']));
 
     // Start building the HTML.
     $html = '<html><body>';
-
+    $imagePath = plugin_dir_path(__FILE__) . '../../../themes/goonj-crm/images/goonj-logo.png';
+    $imageData = base64_encode(file_get_contents($imagePath));
     // Header: Material Receipt#<activity_id> (left) | Goonj address (right)
     $html .= '<table width="100%" cellpadding="0" cellspacing="0" style="border: none;">';
     $html .= '<tr>';
     $html .= '<td style="text-align: left; vertical-align: top;">Material Receipt#' . $activity['id'] . '</td>';
+    $html .= '<img class="w-150" src="data:image/png;base64,' . $imageData . '" alt="Goonj-logo">';
     $html .= '<td style="text-align: right; vertical-align: top;">Goonj, C-544, Pocket C, Sarita Vihar, Delhi, India</td>';
     $html .= '</tr>';
     $html .= '</table>';
@@ -113,18 +151,17 @@ class MaterialContributionService extends AutoSubscriber {
 
     $html .= '<tr>';
     $html .= '<td>Address</td>';
-    $html .= '<td>B-38, Sarita Vihar, Delhi, Sarita Vihar, DELHI, 110076, India(TODO)</td>';
+    $html .= '<td>' . $address . '</td>';
     $html .= '</tr>';
 
     $html .= '<tr>';
     $html .= '<td>Email</td>';
-    $html .= '<td>' . $activity['contact.display_name'] . '(TODO)</td>';
+    $html .= '<td>' . $email . '</td>';
     $html .= '</tr>';
 
     $html .= '<tr>';
     $html .= '<td>Phone</td>';
-    // Hardcoded phone number.
-    $html .= '<td>9876543210 (TODO)</td>';
+    $html .= '<td>' . $phone . '</td>';
     $html .= '</tr>';
 
     $html .= '<tr>';
