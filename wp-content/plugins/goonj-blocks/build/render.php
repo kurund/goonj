43f569a27abc541333b199b01e2eb7cfbdaaef7a
Nishant Kumar <nishant.kumar@coloredcow.in>
2024-09-19 10:50:34 +0530

    Removed donate button

--- wp-content/plugins/goonj-blocks/build/render.php
+++ wp-content/plugins/goonj-blocks/build/render.php
@@ -35,8 +35,8 @@ $pu_material_contribution_check_link = sprintf(
 );
 
 if ( in_array( $target, array( 'collection-camp', 'dropping-center' ) ) ) :
-	$start_date = new DateTime( $action_target['start_date'] );
-	$end_date   = new DateTime( $action_target['end_date'] );
+	$start_date = new DateTime( $action_target['Collection_Camp_Intent_Details.Start_Date'] );
+	$end_date   = new DateTime( $action_target['Collection_Camp_Intent_Details.End_Date'] );
 	?>
 	<div class="wp-block-gb-heading-wrapper">
 		<h2 class="wp-block-gb-heading"><?php echo esc_html($heading_text); ?></h2>
@@ -76,9 +76,6 @@ if ( in_array( $target, array( 'collection-camp', 'dropping-center' ) ) ) :
 			</a>
 			<a href="<?php echo esc_url( $pu_material_contribution_check_link ); ?>" class="wp-block-gb-action-button">
 				<?php esc_html_e( 'Material Contribution', 'goonj-blocks' ); ?>
-			</a>
-			<a href="" class="wp-block-gb-action-button">
-				<?php esc_html_e( 'Donate', 'goonj-blocks' ); ?>
 			</a>
 		</div>
 <?php endif;
