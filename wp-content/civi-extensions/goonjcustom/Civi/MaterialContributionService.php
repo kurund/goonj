516cbd95d9dee82705bcc65d5ec775d391fe61ba
belwalshubham <belwalshubham08@gmail.com>
2024-09-20 11:59:46 +0530

    fix)*): cleanups

--- wp-content/civi-extensions/goonjcustom/Civi/MaterialContributionService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/MaterialContributionService.php
@@ -140,8 +140,7 @@ class MaterialContributionService extends AutoSubscriber {
             Goonj, C-544, Pocket C, Sarita Vihar, Delhi, India
           </div>
         </div>
-        <br><br>
-        <div style="font-weight: bold; font-style: italic; margin-top: 16px; margin-bottom: 4px;">
+        <div style="font-weight: bold; font-style: italic; margin-top: 16px; margin-bottom: 6px;">
           "We appreciate your contribution of pre-used/new material. Goonj makes sure that the material reaches people with dignity and care."
         </div>
         <table border="1" cellpadding="10" cellspacing="0" style="width: 100%; border-collapse: collapse;">
