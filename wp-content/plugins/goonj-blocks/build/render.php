be0fbe09937ce282c672b56ffe1f9eb9ccfbede9
belwalshubham <belwalshubham08@gmail.com>
2024-09-27 19:27:57 +0530

    fix(*): update collection camp ui

--- wp-content/plugins/goonj-blocks/build/render.php
+++ wp-content/plugins/goonj-blocks/build/render.php
@@ -36,8 +36,11 @@ $pu_material_contribution_check_link = sprintf(
 );
 
 if ( in_array( $target, array( 'collection-camp', 'dropping-center' ) ) ) :
+	error_log("Collection_Camp_Intent_Details: " . print_r($action_target, TRUE));
 	$start_date = new DateTime( $action_target['Collection_Camp_Intent_Details.Start_Date'] );
 	$end_date   = new DateTime( $action_target['Collection_Camp_Intent_Details.End_Date'] );
+	$address = $action_target['Collection_Camp_Intent_Details.Location_Area_of_camp'];
+
 	?>
 	<div class="wp-block-gb-heading-wrapper">
 		<h2 class="wp-block-gb-heading"><?php echo esc_html($heading_text); ?></h2>
@@ -45,21 +48,25 @@ if ( in_array( $target, array( 'collection-camp', 'dropping-center' ) ) ) :
 	<table class="wp-block-gb-table">
 		<tbody>
 			<tr class="wp-block-gb-table-row">
-				<td class="wp-block-gb-table-cell wp-block-gb-table-header">Start date</td>
+				<td class="wp-block-gb-table-cell wp-block-gb-table-header">From</td>
 				<td class="wp-block-gb-table-cell"><?php echo $start_date->format( 'd-m-Y h:i A' ); ?></td>
 			</tr>
 			<tr class="wp-block-gb-table-row">
-				<td class="wp-block-gb-table-cell wp-block-gb-table-header">End date</td>
+				<td class="wp-block-gb-table-cell wp-block-gb-table-header">To</td>
 				<td class="wp-block-gb-table-cell"><?php echo $end_date->format( 'd-m-Y h:i A' ); ?></td>
+			</tr>
+			<tr class="wp-block-gb-table-row">
+				<td class="wp-block-gb-table-cell wp-block-gb-table-header">Address of the camp</td>
+				<td class="wp-block-gb-table-cell"><?php echo esc_html( $address ); ?></td>
 			</tr>
 		</tbody>
 	</table>
 	<div <?php echo get_block_wrapper_attributes(); ?>>
 		<a href="<?php echo esc_url( $register_link ); ?>" class="wp-block-gb-action-button">
-			<?php esc_html_e( 'Register', 'goonj-blocks' ); ?>
+			<?php esc_html_e( 'Volunteer with Goonj', 'goonj-blocks' ); ?>
 		</a>
 		<a href="<?php echo esc_url( $material_contribution_link ); ?>" class="wp-block-gb-action-button">
-			<?php esc_html_e( 'Material Contribution', 'goonj-blocks' ); ?>
+			<?php esc_html_e( 'Record your Material Contribution', 'goonj-blocks' ); ?>
 		</a>
 	</div>
 	<?php elseif ( 'processing-center' === $target ) : ?>
